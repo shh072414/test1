78bea8c04095ddafb0c564b15919028c6840063c
shh072414 <48975628+shh072414@users.noreply.github.com>
2020-06-08 23:40:35 +0800

    Revert "1.3"
    This reverts commit 05365448d646c37e46f9b46646a8381eb685fda8.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -16,7 +16,7 @@ namespace ConsoleApplication1
             string filepath = @"D:\test.ini";
             string qq = string.Empty;
             int aa = GetPrivateProfileSectionA("ccc",bb,1024,filepath);
-            //1.3
+            //1.2
             Console.WriteLine(bb.ToString());
             Console.Read();
 
